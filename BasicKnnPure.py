8725182416142be8128cbaddc489f3822d22dbe1
Ali Sadeghi Aghili <alisadeghiaghili@gmail.com>
2022-02-27 17:36:43 +0330

    Update BasicKnnPure.py

--- BasicKnnPure.py
+++ BasicKnnPure.py
@@ -5,12 +5,27 @@ def distance(point1, point2):
   final_distance = squared_difference ** 0.5
   return final_distance
 
-def classify(unknownPoint, dataset, k):
+def classify(unknown, dataset, labels, k):
   distances = []
-  for title in dataset:
-    distance_to_point = distance(unknownPoint, dataset[title])
-    distances.append([distance_to_point, title])
   
+  for title in dataset:
+    movie = dataset[title]
+    distance_to_point = distance(movie, unknown)
+    distances.append([distance_to_point, title])
   distances.sort()
   neighbors = distances[:k]
-  return neighbors
+  
+  num_good = 0
+  num_bad = 0
+  
+  for neighbor in neighbors:
+    title = neighbor[1]
+    if labels[title] == 0:
+      num_bad += 1
+    elif labels[title] == 1:
+      num_good += 1
+      
+  if num_good > num_bad:
+    return 1
+  else:
+    return 0
