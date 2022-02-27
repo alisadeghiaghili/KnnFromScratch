733f5941f978bd7297fd82409498f6afde58a999
Ali Sadeghi Aghili <alisadeghiaghili@gmail.com>
2022-02-27 15:50:24 +0330

    Create BasicKnnPure.py

---
+++ BasicKnnPure.py
@@ -0,0 +1,16 @@
+def distance(point1, Point2):
+  squared_difference = 0
+  for i in range(len(point1)):
+    squared_difference += (point1[i] - point2[i]) ** 2
+  final_distance = squared_difference ** 0.5
+  return final_distance
+
+def classify(unknownPoint, dataset, k):
+  distances = []
+  for title in dataset:
+    distance_to_point = distance(unknownPoint, dataset[title])
+    distances.append([distance_to_point, title])
+  
+  distances.sort()
+  neighbors = distances[:k]
+  return neighbors
